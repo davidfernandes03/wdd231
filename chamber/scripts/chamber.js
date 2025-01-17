cdc307b68bcf304adbe4f8d1524edfd27d61e517
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2025-01-17 17:18:39 +0000

    Chamber Directory

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,71 @@
+// HeaderHamburger Element
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('#animation');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('open');
+    hamburger.classList.toggle('open');
+});
+
+// Get current year
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+// Get last modification
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`
+
+// Toggle between grid and list view
+document.getElementById('grid-view').addEventListener('click', () => {
+    document.querySelector('.directory-content').classList.add('grid');
+    document.querySelector('.directory-content').classList.remove('list');
+});
+document.getElementById('list-view').addEventListener('click', () => {
+    document.querySelector('.directory-content').classList.add('list');
+    document.querySelector('.directory-content').classList.remove('grid');
+});
+
+// Fetch members.json file
+async function fetchMembers() {
+    try {
+        const response = await fetch('data/members.json');
+        const members = await response.json();
+
+        displayMembers(members);
+    } catch (error) {
+        console.error(`Error fetching member data: ${error}`);
+    }
+}
+
+function displayMembers(members) {
+    const directoryContent = document.querySelector('.directory-content');
+    directoryContent.innerHTML = '';
+
+    members.forEach(member => {
+        const memberCard = document.createElement('div');
+        memberCard.classList.add('member-card');
+
+        memberCard.innerHTML = `
+            <section>
+                <img src="images/${member.image}" alt="${member.name} Logo">
+                <h3>${member.name}</h3>
+                <p id="address">${member.address}</p>
+                <p>${member.phone}</p>
+                <a href="${member.website}" target="_blank">Visit Website</a>
+                <p id="member"><strong>Membership Level: <br id="for-list">${getMembershipLevel(member.membership)}</strong></p>
+                <p id="descript">${member.description}</p>
+            </section>
+        `;
+
+        directoryContent.appendChild(memberCard);
+    });
+}
+
+function getMembershipLevel(level) {
+    switch (level) {
+        case 1: return 'Member';
+        case 2: return 'Silver';
+        case 3: return 'Gold';
+        default: return 'Unknown';
+    }
+}
+
+fetchMembers();
