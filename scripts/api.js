1773c57b5c0df1f36f3dfc881b593efe8ad74ecd
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2025-01-21 21:14:37 +0000

    Api Exercise

---
+++ scripts/api.js
@@ -0,0 +1,37 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// A chave precisa estar entre aspas.
+const key = 'c3541ee671c135aefd228b58ec7a3d7b';
+const lat = 49.7503532128715;
+const lon = 6.630917405585122;
+
+// A versão correta da API para dados atuais geralmente é 2.5, não 3.0.
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+apiFetch();
