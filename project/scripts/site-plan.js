b56cfe643f8c72f203533404b463152f02473fe3
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2025-02-05 15:00:08 +0000

    Project Site Plan

---
+++ project/scripts/site-plan.js
@@ -0,0 +1,5 @@
+// Get current year
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+// Get last modification
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`
